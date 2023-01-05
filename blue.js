1b73390b60d3c18732d48b14a5947c405b0901cb
Typonomy <lqconnor@alaska.edu>
2023-01-05 03:15:34 -0500

    Add files via upload

--- blue.js
+++ blue.js
@@ -1,2 +1,2 @@
-document.body.style.backgroundImage = "url('https://github.com/Typonomy/RealTwitterBlue/blob/main/images/Rain7.jpg')";
+document.body.style.backgroundImage = "url('https://github.com/Typonomy/RealTwitterBlue/blob/765189cc1e4f6ee40a9d2546aeac774dff3f8d70/images/Rain7.jpg')";
 document.body.style.backgroundAttachment = "fixed";
